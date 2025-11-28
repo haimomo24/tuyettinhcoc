57c09875b5707c36c432524ed4cbd386f381523c
haimomo24 <135099315+haimomo24@users.noreply.github.com>
2025-11-28 09:23:26 +0700

    push

--- src/app/vi/layout.js
+++ src/app/vi/layout.js
@@ -3,7 +3,26 @@ import HeaderPagevi from "../component/vi/layout/HeaderPagevi";
 import "../globals.css";
 
 export const metadata = {
-  title: "Tuyệt Tịnh Cốc ",
+  title: "Khu Du Lịch Tuyệt Tịnh Cốc Ninh Bình",
+  description: "Khu Du Lịch Tuyệt Tịnh Cốc Ninh Bình",
+
+  openGraph: {
+    title: "Khu Du Lịch Tuyệt Tịnh Cốc Ninh Bình",
+    description: "Khu Du Lịch Tuyệt Tịnh Cốc Ninh Bình",
+    url: "https://tuyettinhcocninhbinh.com/vi",
+    siteName: "Tuyệt Tịnh Cốc Ninh Bình",
+    locale: "vi_VN",
+    type: "website",
+    images: [
+      {
+        url: "https://tuyettinhcocninhbinh.com/og.jpg", // thay bằng ảnh thật
+        width: 1200,
+        height: 630,
+        alt: "Tuyệt Tịnh Cốc Ninh Bình",
+      },
+    ],
+  },
+
   icons: {
     icon: [
       { url: "/images/logo-bai-dinh.png", type: "image/png" },
@@ -16,11 +35,8 @@ export default function ViLayout({ children }) {
   return (
     <>
       <HeaderPagevi />
-
       <div className="h-[100px] w-full"></div>
-
       {children}
-
       <FooterPage />
     </>
   );
