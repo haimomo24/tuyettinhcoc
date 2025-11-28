42a4d9d8558b96d5efc769c730d0a8790e693bb4
haimomo24 <135099315+haimomo24@users.noreply.github.com>
2025-11-28 15:32:03 +0700

    pussh

---
+++ src/app/metadata.js
@@ -0,0 +1,22 @@
+export const metadata = {
+  title: "Khu Du Lịch Tuyệt Tịnh Cốc Ninh Bình",
+  description: "Khu Du Lịch Tuyệt Tịnh Cốc Ninh Bình",
+  openGraph: {
+    title: "Khu Du Lịch Tuyệt Tịnh Cốc Ninh Bình",
+    description: "Khu Du Lịch Tuyệt Tịnh Cốc Ninh Bình",
+    url: "https://tuyettinhcocninhbinh.com",
+    siteName: "Tuyệt Tịnh Cốc Ninh Bình",
+    locale: "vi_VN",
+    type: "website",
+    images: [
+      "https://tuyettinhcocninhbinh.com/image/logo/505107164797192833.jpg"
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Khu Du Lịch Tuyệt Tịnh Cốc Ninh Bình",
+    images: [
+      "https://tuyettinhcocninhbinh.com/image/logo/505107164797192833.jpg",
+    ],
+  },
+};
