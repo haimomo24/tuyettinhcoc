0a87c4e536daac3446a9e2a5ceaeea36e60795b3
haimomo24 <135099315+haimomo24@users.noreply.github.com>
2025-11-28 09:32:48 +0700

    push

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  poweredByHeader: false,
+  reactStrictMode: true,
+  reactCompiler: true,
 };
 
 export default nextConfig;
